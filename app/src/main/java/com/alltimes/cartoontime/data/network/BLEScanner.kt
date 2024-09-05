8b3c1e0e77d9b531ea55668c65c02505f1358991
백승우 <swthewhite@gmail.com>
2024-09-05 11:00:18 +0900

    [#3] BLE Scanner 리팩토링

--- app/src/main/java/com/alltimes/cartoontime/data/network/BLEScanner.kt
+++ app/src/main/java/com/alltimes/cartoontime/data/network/BLEScanner.kt
@@ -11,60 +11,54 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 import kotlin.math.pow
 
-// These fields are marked as API >= 31 in the Manifest class, so we can't use those without warning.
-// So we create our own, which prevents over-suppression of the Linter
-const val PERMISSION_BLUETOOTH_SCAN = "android.permission.BLUETOOTH_SCAN"
-const val PERMISSION_BLUETOOTH_CONNECT = "android.permission.BLUETOOTH_CONNECT"
-
-// Data class to store device information
-data class DeviceInfo(
-    val device: BluetoothDevice,
-    val distance: Double
-)
-
 class BLEScanner(context: Context) {
-    private val bluetooth = context.getSystemService(Context.BLUETOOTH_SERVICE)
+    // Companion object를 이용하여 상수 정의
+    companion object {
+        const val PERMISSION_BLUETOOTH_SCAN = "android.permission.BLUETOOTH_SCAN"
+        const val PERMISSION_BLUETOOTH_CONNECT = "android.permission.BLUETOOTH_CONNECT"
+    }
+
+    // BluetoothManager를 이용하여 BluetoothAdapter와 BluetoothLeScanner를 초기화
+    private val bluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE)
             as? BluetoothManager
-        ?: throw Exception("Bluetooth is not supported by this device")
+        ?: throw Exception("이 디바이스는 Bluetooth 지원하지 않습니다")
+    private val bluetoothAdapter = bluetoothManager.adapter
+    private val scanner: BluetoothLeScanner
+        get() = bluetoothAdapter.bluetoothLeScanner
 
+    // 스캔 중인지 여부를 저장하는 MutableStateFlow
     val isScanning = MutableStateFlow(false)
-
+    // 찾은 디바이스 목록을 저장하는 MutableStateFlow
     val foundDevices = MutableStateFlow<List<DeviceInfo>>(emptyList())
 
-    private val scanner: BluetoothLeScanner
-        get() = bluetooth.adapter.bluetoothLeScanner
+    // 이름 없는 디바이스는 리스트에 추가하지 않는 함수
+    private fun processScanResult(result: ScanResult?): DeviceInfo? {
+        result ?: return null
+        val deviceName = result.device.name ?: return null
+        if (deviceName.isEmpty()) return null
+
+        val distance = calculateDistance(result.rssi, result.txPower)
+        return DeviceInfo(result.device, distance)
+    }
 
     private val scanCallback = object : ScanCallback() {
+        // 스캔 결과를 처리하는 함수
         override fun onScanResult(callbackType: Int, result: ScanResult?) {
             super.onScanResult(callbackType, result)
-            result ?: return
 
-            val deviceName = result.device.name
-
-            // 이름이 없는 디바이스는 리스트에 추가하지 않음
-            if (deviceName != null && deviceName.isNotEmpty()) {
-                val distance = calculateDistance(result.rssi, result.txPower)
-                val deviceInfo = DeviceInfo(result.device, distance)
-                if (!foundDevices.value.any { it.device.address == result.device.address }) {
+            processScanResult(result)?.let { deviceInfo ->
+                if (foundDevices.value.none { it.device.address == deviceInfo.device.address }) {
                     foundDevices.update { it + deviceInfo }
                 }
             }
         }
 
+        // 여러 개의 스캔 결과를 처리하는 함수
         override fun onBatchScanResults(results: MutableList<ScanResult>?) {
             super.onBatchScanResults(results)
             results ?: return
 
-            val newDevices = results.mapNotNull { result ->
-                val deviceName = result.device.name
-
-                if (deviceName != null && deviceName.isNotEmpty()) {
-                    val distance = calculateDistance(result.rssi, result.txPower)
-                    DeviceInfo(result.device, distance)
-                } else {
-                    null
-                }
-            }
+            val newDevices = results.mapNotNull { processScanResult(it) }
                 .filterNot { foundDevices.value.any { info -> info.device.address == it.device.address } }
 
             if (newDevices.isNotEmpty()) {
@@ -72,30 +66,34 @@ class BLEScanner(context: Context) {
             }
         }
 
+        // 스캔이 실패했을 때 호출되는 함수
         override fun onScanFailed(errorCode: Int) {
             super.onScanFailed(errorCode)
             isScanning.value = false
         }
     }
 
+    // 스캔을 시작하는 함수, ScanCallback을 이용하여 스캔 결과를 처리
     @RequiresPermission(PERMISSION_BLUETOOTH_SCAN)
     fun startScanning() {
         scanner.startScan(scanCallback)
         isScanning.value = true
     }
 
+    // 스캔을 중지하는 함수
     @RequiresPermission(PERMISSION_BLUETOOTH_SCAN)
     fun stopScanning() {
         scanner.stopScan(scanCallback)
         isScanning.value = false
     }
 
-    // RSSI와 txPower를 이용하여 대략적인 거리를 계산하는 함수
+    // 대략적인 거리를 계산하는 함수, RSSI와 txPower를 이용하여 계산
     private fun calculateDistance(rssi: Int, txPower: Int): Double {
-        if (txPower == Integer.MIN_VALUE) {
-            // txPower 값이 없는 경우 RSSI만으로는 정확한 계산이 불가능하여 일단 기본 거리를 반환
-            return -1.0
-        }
+        // txPower 값이 없는 경우,
+        // RSSI만으로는 정확한 계산이 불가능하여,
+        // 일단 기본 거리를 반환
+        if (txPower == Integer.MIN_VALUE)  return -1.0
+
         val ratio = rssi * 1.0 / txPower
         return if (ratio < 1.0) {
             ratio.pow(10.0)
@@ -104,3 +102,9 @@ class BLEScanner(context: Context) {
         }
     }
 }
+
+// 디바이스 정보를 저장하는 데이터 클래스
+data class DeviceInfo(
+    val device: BluetoothDevice,
+    val distance: Double
+)
