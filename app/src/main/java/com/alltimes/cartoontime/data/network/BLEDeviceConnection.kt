77c5083a99fdab5b91da06686b89f1c3b1da1f18
백승우 <swthewhite@gmail.com>
2024-09-06 20:35:29 +0900

    [#3] GATT 서비스를 못 찾는 버그 해결 (비동기->절차식 수정)

--- app/src/main/java/com/alltimes/cartoontime/data/network/BLEDeviceConnection.kt
+++ app/src/main/java/com/alltimes/cartoontime/data/network/BLEDeviceConnection.kt
@@ -14,11 +14,15 @@ import java.util.UUID
 import com.alltimes.cartoontime.data.model.Permissions
 import com.alltimes.cartoontime.data.model.BLEConstants
 
+val CTF_SERVICE_UUID: UUID = UUID.fromString("8c380000-10bd-4fdb-ba21-1922d6cf860d")
+val PASSWORD_CHARACTERISTIC_UUID: UUID = UUID.fromString("8c380001-10bd-4fdb-ba21-1922d6cf860d")
+val NAME_CHARACTERISTIC_UUID: UUID = UUID.fromString("8c380002-10bd-4fdb-ba21-1922d6cf860d")
+
 @Suppress("DEPRECATION")
 class BLEDeviceConnection @RequiresPermission("PERMISSION_BLUETOOTH_CONNECT") constructor(
     // Context와 BluetoothDevice를 받아서 초기화
     private val context: Context,
-    private val bluetoothDevice: BluetoothDevice
+    private val bluetoothDevice: DeviceInfo
 ) {
     // 연결 상태를 저장하는 MutableStateFlow
     val isConnected = MutableStateFlow(false)
@@ -30,6 +34,11 @@ class BLEDeviceConnection @RequiresPermission("PERMISSION_BLUETOOTH_CONNECT") co
     val services = MutableStateFlow<List<BluetoothGattService>>(emptyList())
     // BluetoothGatt 객체를 저장하는 변수
     private var gatt: BluetoothGatt? = null
+    private val uwbCommunicator = UWBControleeManger(context)
+
+    val serviceDiscoveryCompleted = MutableStateFlow(false)
+    val passwordReadCompleted = MutableStateFlow(false)
+    val nameWrittenCompleted = MutableStateFlow(false)
 
     // BluetoothGattCallback을 상속받은 callback 객체
     @RequiresPermission(Permissions.BLUETOOTH_CONNECT)
@@ -46,8 +55,17 @@ class BLEDeviceConnection @RequiresPermission("PERMISSION_BLUETOOTH_CONNECT") co
         @RequiresPermission(Permissions.BLUETOOTH_CONNECT)
         override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
             super.onServicesDiscovered(gatt, status)
-            // GATT 서비스 목록을 업데이트
-            services.value = gatt.services
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                services.value = gatt.services
+                serviceDiscoveryCompleted.value = true
+                Log.d("BLE", "Services discovered: ${gatt.services.size} services found.")
+                for (service in gatt.services) {
+                    Log.d("BLE", "Service UUID: ${service.uuid}")
+                }
+            } else {
+                Log.e("BLE", "Service discovery failed with status: $status")
+                serviceDiscoveryCompleted.value = false
+            }
         }
 
         // 읽은 데이터를 처리하는 함수
@@ -60,7 +78,7 @@ class BLEDeviceConnection @RequiresPermission("PERMISSION_BLUETOOTH_CONNECT") co
         ) {
             super.onCharacteristicRead(gatt, characteristic, status)
             // 읽은 데이터가 CONTROLEE_CHARACTERISTIC_UUID인 경우 dataBLERead에 저장
-            handleCharacteristicRead(characteristic)
+            handleCharacteristicRead(characteristic, status)
         }
 
         // 쓴 데이터를 처리하는 함수
@@ -72,7 +90,7 @@ class BLEDeviceConnection @RequiresPermission("PERMISSION_BLUETOOTH_CONNECT") co
         ) {
             super.onCharacteristicWrite(gatt, characteristic, status)
             // 쓴 데이터가 CONTROLLER_CHARACTERISTIC_UUID인 경우 successfulDataWrites를 업데이트
-            handleCharacteristicWrite(characteristic)
+            handleCharacteristicWrite(characteristic, status)
         }
     }
 
@@ -88,24 +106,39 @@ class BLEDeviceConnection @RequiresPermission("PERMISSION_BLUETOOTH_CONNECT") co
 
     // 특성 읽기 처리 함수
     @RequiresPermission(Permissions.BLUETOOTH_CONNECT)
-    private fun handleCharacteristicRead(characteristic: BluetoothGattCharacteristic) {
+    private fun handleCharacteristicRead(characteristic: BluetoothGattCharacteristic, status: Int) {
         if (characteristic.uuid == BLEConstants.CONTROLEE_CHARACTERISTIC_UUID) {
             dataBLERead.value = String(characteristic.value)
+            Log.d("BLE", "Characteristic read successful: ${dataBLERead.value}")
+            passwordReadCompleted.value = true
+        } else {
+            Log.e("BLE", "Characteristic read failed for UUID: ${characteristic.uuid}, status: $status")
+            passwordReadCompleted.value = false
         }
     }
 
     // 특성 쓰기 처리 함수
     @RequiresPermission(Permissions.BLUETOOTH_CONNECT)
-    private fun handleCharacteristicWrite(characteristic: BluetoothGattCharacteristic) {
+    private fun handleCharacteristicWrite(characteristic: BluetoothGattCharacteristic, status: Int) {
         if (characteristic.uuid == BLEConstants.CONTROLLER_CHARACTERISTIC_UUID) {
-            successfulDataWrites.update { it + 1 }
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                successfulDataWrites.update { it + 1 }
+                Log.d("BLE", "Characteristic write successful: ${characteristic.uuid}")
+
+                // Notify that the write was successful
+                nameWrittenCompleted.value = true
+            } else {
+                Log.e("BLE", "Characteristic write failed for UUID: ${characteristic.uuid}, status: $status")
+                // Notify that the write failed
+                nameWrittenCompleted.value = false
+            }
         }
     }
 
     // GATT 연결을 위한 함수
     @RequiresPermission(Permissions.BLUETOOTH_CONNECT)
     fun connect() {
-        gatt = bluetoothDevice.connectGatt(context, false, callback)
+        gatt = bluetoothDevice.device.connectGatt(context, false, callback)
     }
 
     // GATT 연결 해제를 위한 함수
@@ -125,11 +158,12 @@ class BLEDeviceConnection @RequiresPermission("PERMISSION_BLUETOOTH_CONNECT") co
     // 특성 읽기 함수
     @RequiresPermission(Permissions.BLUETOOTH_CONNECT)
     fun readCharacteristic() {
+        Log.d("bluetooth", "ReadCharacteristic")
         val service = gatt?.getService(BLEConstants.UWB_SERVICE_UUID)
         val characteristic = service?.getCharacteristic(BLEConstants.CONTROLEE_CHARACTERISTIC_UUID)
         if (characteristic != null) {
             val success = gatt?.readCharacteristic(characteristic)
-            Log.v("bluetooth", "Read status: $success")
+            Log.v("bluetooth", "Read status: $success, ${characteristic.value}")
         }
     }
 
@@ -139,9 +173,10 @@ class BLEDeviceConnection @RequiresPermission("PERMISSION_BLUETOOTH_CONNECT") co
         val service = gatt?.getService(BLEConstants.UWB_SERVICE_UUID)
         val characteristic = service?.getCharacteristic(BLEConstants.CONTROLLER_CHARACTERISTIC_UUID)
         if (characteristic != null) {
-            characteristic.value = "CONTROLLER".toByteArray()
+            val uwbAddress = uwbCommunicator.getUwbAddress()
+            characteristic.value = uwbAddress.toByteArray()
             val success = gatt?.writeCharacteristic(characteristic)
-            Log.v("bluetooth", "Write status: $success")
+            Log.v("bluetooth", "Write status: $success, ${characteristic.value}")
         }
     }
 }
--- app/src/main/java/com/alltimes/cartoontime/ui/viewmodel/SendViewModel.kt
+++ app/src/main/java/com/alltimes/cartoontime/ui/viewmodel/SendViewModel.kt
@@ -4,14 +4,24 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.bluetooth.BluetoothDevice
 import android.util.Log
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.alltimes.cartoontime.data.network.BLEDeviceConnection
 import com.alltimes.cartoontime.data.network.BLEScanner
+import com.alltimes.cartoontime.data.network.DeviceInfo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.withContext
 
 // UI 상태를 저장하는 데이터 클래스
 data class SendUiState(
@@ -25,14 +35,30 @@ data class SendUiState(
     val successfulDataWrites: Int = 0
 )
 
-class SendViewModel(application: Application) : AndroidViewModel(application) {
+class SendViewModel(private val application: Application) : AndroidViewModel(application) {
+
 
     private val bleScanner = BLEScanner(application)
-    private var activeConnection = MutableStateFlow<BLEDeviceConnection?>(null)
 
-    // 상태를 저장하는 StateFlow
+    // Now activeConnection and dataBLERead are MutableStateFlow
+    private val _activeConnection = MutableStateFlow<BLEDeviceConnection?>(null)
+    private val dataBLERead = MutableStateFlow<String?>(null)
+
+    private val triedDevices = mutableSetOf<String>()
+
+    // StateFlow for UI state
     private val _uiState = MutableStateFlow(SendUiState())
-    val uiState: StateFlow<SendUiState> = _uiState
+    val uiState = combine(
+        _uiState,
+        _activeConnection,
+        dataBLERead,
+    ) { state, connection, dataRead ->
+        state.copy(
+            isDeviceConnected = connection != null && connection.isConnected.value,
+            dataBLERead = dataRead
+        )
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), SendUiState())
+
 
     // 버튼 클릭 시 호출되는 함수
     fun onSendButtonClick() {
@@ -49,60 +75,106 @@ class SendViewModel(application: Application) : AndroidViewModel(application) {
     @SuppressLint("MissingPermission")
     fun startScanningAndConnect() {
         viewModelScope.launch {
-            Log.d("SendViewModel", "startScanningAndConnect")
-            bleScanner.startScanning()
+            Log.d("SendViewModel", "Scanning started")
             _uiState.update { it.copy(isScanning = true) }
 
-            bleScanner.foundDevices.collect { devicesInfo ->
-                if (devicesInfo.isNotEmpty()) {
-                    val deviceInfo = devicesInfo.firstOrNull()
-                    if (deviceInfo != null) {
-                        setActiveDevice(deviceInfo.device)
-                        connectToDeviceAndPerformActions()
+            CoroutineScope(Dispatchers.Main).launch {
+                bleScanner.startScanning()
+
+                bleScanner.foundDevices.collectLatest { devices ->
+                    for (deviceInfo in devices) {
+                        if (!triedDevices.contains(deviceInfo.device.address)) {
+                            connectToDeviceSequentially(deviceInfo)
+                        }
                     }
-                    bleScanner.stopScanning()
-                    _uiState.update { it.copy(isScanning = false) }
                 }
             }
         }
     }
 
-    private fun setActiveDevice(device: BluetoothDevice) {
-        activeConnection.value = BLEDeviceConnection(getApplication(), device)
-        _uiState.update { it.copy(activeDevice = device) }
-    }
+    @SuppressLint("MissingPermission")
+    private suspend fun connectToDeviceSequentially(deviceInfo: DeviceInfo) {
+        // Add device to the tried list
+        triedDevices.add(deviceInfo.device.address)
 
-    private fun connectToDeviceAndPerformActions() {
-        activeConnection.value?.let { connection ->
-            connection.connect()
+        _activeConnection.value = BLEDeviceConnection(application, deviceInfo)
+        val activeConnection: BLEDeviceConnection? = _activeConnection.value
 
-            viewModelScope.launch {
-                connection.discoverServices()
-                connection.readCharacteristic()
-                connection.writeCharacteristic()
+        withContext(Dispatchers.Main) {
+            activeConnection?.connect()
 
-                _uiState.update { uiState ->
-                    uiState.copy(
-                        isDeviceConnected = true,
-                        discoveredCharacteristics = connection.services.value
-                            .associate { it.uuid.toString() to it.characteristics.map { it.uuid.toString() } },
-                        dataBLERead = connection.dataBLERead.value,
-                        successfulDataWrites = connection.successfulDataWrites.value
-                    )
+            // Ensure we wait until the device is connected
+            activeConnection?.isConnected?.collectLatest { isConnected ->
+                if (isConnected) {
+                    Log.w("BLEConnection", "Connected to device: ${deviceInfo.device.name}")
+
+                    // Wait for service discovery to complete
+                    CoroutineScope(Dispatchers.Main).launch {
+                        activeConnection?.discoverServices()
+                        activeConnection?.serviceDiscoveryCompleted?.collectLatest { servicesDiscovered ->
+                            if (servicesDiscovered) {
+                                Log.d("BLEConnection", "Services discovered successfully.")
+
+                                // Wait for password reading to complete
+                                activeConnection?.readCharacteristic()
+                                activeConnection?.passwordReadCompleted?.collectLatest { dataBLERead ->
+                                    if (dataBLERead) {
+                                        Log.d("BLEConnection", "Password read successfully.")
+
+                                        // Wait for name writing to complete
+                                        activeConnection?.writeCharacteristic()
+                                        activeConnection?.nameWrittenCompleted?.collectLatest { nameWritten ->
+                                            if (nameWritten) {
+                                                Log.d("BLEConnection", "Name written successfully.")
+                                                _uiState.update {
+                                                    it.copy(
+                                                        isDeviceConnected = true,
+                                                        activeDevice = deviceInfo.device
+                                                    )
+                                                }
+                                                Log.d("BLEConnection", "$dataBLERead $nameWritten")
+                                            } else {
+                                                Log.e("BLEConnection", "Failed to write name.")
+                                            }
+                                        }
+                                    } else {
+                                        Log.e("BLEConnection", "Failed to read password.")
+                                    }
+                                }
+                            } else {
+                                Log.e("BLEConnection", "Failed to discover services.")
+                            }
+                        }
+                    }
+                } else {
+                    Log.d("BLEConnection", "Failed to connect to device: ${deviceInfo.device.name}")
                 }
             }
         }
     }
 
+    @SuppressLint("MissingPermission")
     private fun disconnectDevice() {
-        activeConnection.value?.disconnect()
+        Log.d("SendViewModel", "Disconnecting device")
+        _activeConnection.value?.disconnect()
         _uiState.update { it.copy(isDeviceConnected = false, activeDevice = null) }
+
+        // Clear triedDevices when disconnecting
+        triedDevices.clear()
+
+        // Ensure scanning is stopped if needed
+        if (_uiState.value.isScanning) {
+            bleScanner.stopScanning()
+            Log.d("SendViewModel", "Scanning stopped after disconnecting")
+        }
     }
 
+    @SuppressLint("MissingPermission")
     override fun onCleared() {
         super.onCleared()
 
         if (bleScanner.isScanning.value) {
+            Log.d("SendViewModel", "Stopping scanning on ViewModel cleared")
             bleScanner.stopScanning()
         }
     }
