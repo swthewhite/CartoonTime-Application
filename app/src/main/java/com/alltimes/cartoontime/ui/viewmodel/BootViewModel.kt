d39a051b268eab0cc48830111cc6a413350664f7
박상빈 <qazw181900@naver.com>
2024-09-06 14:11:50 +0900

    [#11] 부트 페이지 UI 구성
    부트 페이지 UI 구성

---
+++ app/src/main/java/com/alltimes/cartoontime/ui/viewmodel/BootViewModel.kt
@@ -0,0 +1,16 @@
+package com.alltimes.cartoontime.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class BootViewModel : ViewModel() {
+
+
+    // 버튼 클릭 이벤트를 처리하는 함수
+    fun onLoginClick() {
+        // 로그인 처리 로직
+    }
+
+    fun onSignUpClick() {
+        // 회원가입 처리 로직
+    }
+}
