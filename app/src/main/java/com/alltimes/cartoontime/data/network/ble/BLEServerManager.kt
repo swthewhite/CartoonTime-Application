100fd56a0d4bc798e4e6e9b356c9468b0a66791c
백승우 <swthewhite@gmail.com>
2024-09-20 13:30:15 +0900

    [#17] BLE Server의 Characteristic 1차 추가

--- app/src/main/java/com/alltimes/cartoontime/data/network/ble/BLEServerManager.kt
+++ app/src/main/java/com/alltimes/cartoontime/data/network/ble/BLEServerManager.kt
@@ -38,8 +38,8 @@ class BLEServerManager(private val context: Context) {
     private val preparedWrites = HashMap<Int, ByteArray>()
     private val deviceNames = mutableMapOf<String, String>()
     val controllerReceived = MutableStateFlow(emptyList<String>())
-    //private val uwbCommunicator = UWBControllerManager(context)
     private val uwbCommunicator = UwbControllerCommunicator(context)
+
 
     @RequiresPermission(allOf = ["android.permission.BLUETOOTH_CONNECT", "android.permission.BLUETOOTH_ADVERTISE"])
     suspend fun startServer() = withContext(Dispatchers.IO) {
@@ -128,7 +128,13 @@ class BLEServerManager(private val context: Context) {
                 super.onCharacteristicReadRequest(device, requestId, offset, characteristic)
                 val uwbAddress = uwbCommunicator.getUwbAddress()
                 val uwbChannel = uwbCommunicator.getUwbChannel()
-                val responseData = "$uwbAddress/$uwbChannel".toByteArray()
+
+                val responseData: ByteArray = when (characteristic?.uuid) {
+                    BLEConstants.CONTROLEE_CHARACTERISTIC_UUID -> "$uwbAddress/$uwbChannel".toByteArray()
+                    BLEConstants.MY_ID_CHARACTERISTIC_UUID -> "ct1298".toByteArray()
+                    else -> "UnknownCharacteristic".encodeToByteArray()
+                }
+
                 server?.sendResponse(device, requestId, BluetoothGatt.GATT_SUCCESS, offset, responseData)
             }
 
@@ -153,9 +159,11 @@ class BLEServerManager(private val context: Context) {
                 )
 
                 if (preparedWrite) {
+                    Log.d("UWB", "Prepared write")
                     val bytes = preparedWrites.getOrDefault(requestId, byteArrayOf())
                     preparedWrites[requestId] = bytes.plus(value)
                 } else {
+                    Log.d("UWB", "Not prepared write")
                     val receivedData = String(value)
                     controllerReceived.update { it.plus(receivedData) }
                     val deviceName = device.name ?: "Unknown Device"
@@ -182,20 +190,34 @@ class BLEServerManager(private val context: Context) {
 
         val service = BluetoothGattService(BLEConstants.UWB_KIOSK_SERVICE_UUID, BluetoothGattService.SERVICE_TYPE_PRIMARY)
 
-        val passwordCharacteristic = BluetoothGattCharacteristic(
+        val controleeCharacteristic = BluetoothGattCharacteristic(
             BLEConstants.CONTROLEE_CHARACTERISTIC_UUID,
             BluetoothGattCharacteristic.PROPERTY_READ,
             BluetoothGattCharacteristic.PERMISSION_READ
         )
 
-        val nameCharacteristic = BluetoothGattCharacteristic(
+        val myIDCharacteristic = BluetoothGattCharacteristic(
+            BLEConstants.MY_ID_CHARACTERISTIC_UUID,
+            BluetoothGattCharacteristic.PROPERTY_READ,
+            BluetoothGattCharacteristic.PERMISSION_READ
+        )
+
+        val controllerCharacteristic = BluetoothGattCharacteristic(
             BLEConstants.CONTROLLER_CHARACTERISTIC_UUID,
             BluetoothGattCharacteristic.PROPERTY_WRITE,
             BluetoothGattCharacteristic.PERMISSION_WRITE
         )
 
-        service.addCharacteristic(passwordCharacteristic)
-        service.addCharacteristic(nameCharacteristic)
+        val partnerIDCharacteristic = BluetoothGattCharacteristic(
+            BLEConstants.PARTNER_ID_CHARACTERISTIC_UUID,
+            BluetoothGattCharacteristic.PROPERTY_WRITE,
+            BluetoothGattCharacteristic.PERMISSION_WRITE
+        )
+
+        service.addCharacteristic(controleeCharacteristic)
+        service.addCharacteristic(controllerCharacteristic)
+        service.addCharacteristic(myIDCharacteristic)
+        service.addCharacteristic(partnerIDCharacteristic)
         server?.addService(service)
         ctfService = service
     }
