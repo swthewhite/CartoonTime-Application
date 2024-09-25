cc255bc2fe0dd830aeef2812eb99564d01c7eec2
박상빈 <qazw181900@naver.com>
2024-09-25 10:18:39 +0900

    [#59] 소나큐브용 커밋
    소나큐브용 커밋

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     id("com.google.gms.google-services")
-    id("org.sonarqube") version "5.0.0.4638"
 }
 
 android {
