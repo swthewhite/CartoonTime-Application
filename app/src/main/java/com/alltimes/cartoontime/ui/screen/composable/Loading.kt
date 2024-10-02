be52042dcdbe51a0e334f8ebf546f0c5f4540e2b
박상빈 <qazw181900@naver.com>
2024-10-02 11:05:19 +0900

    [#77] 로딩 이미지 수정
    인터넷 연결 로딩 이미지 수정

--- app/src/main/java/com/alltimes/cartoontime/ui/screen/composable/Loading.kt
+++ app/src/main/java/com/alltimes/cartoontime/ui/screen/composable/Loading.kt
@@ -2,12 +2,16 @@ package com.alltimes.cartoontime.ui.screen.composable
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
@@ -27,18 +31,22 @@ fun Loading(isLoading: Boolean, onDismiss: () -> Unit) {
             text = {
                 Box(
                     modifier = Modifier
-                        .width(200.dp)
-                        .height(400.dp)
-                        .background(color = Color.White),
+                        .fillMaxWidth()
+                        .background(color = Color(0xFFF4F2EE), shape = RoundedCornerShape(16.dp))
+                        .padding(16.dp),
                     contentAlignment = Alignment.Center
                 ) {
-                    CircularProgressIndicator(
-                        modifier = Modifier.size(48.dp) // 스피너 크기 조정
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Text(
-                        text = "인터넷 연결 시도 중 ..."
-                    )
+                    Column {
+                        CircularProgressIndicator(
+                            modifier = Modifier.size(48.dp) // 스피너 크기 조정
+                        )
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        Text(
+                            text = "인터넷 연결 시도 중 ..."
+                        )
+                    }
                 }
             },
             confirmButton = {
